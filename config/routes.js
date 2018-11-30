7ee0ca5bf85ff95662dbb5922656f82cce404f17
Axolotl <samuelmachat@gmail.com>
2018-11-30 12:16:03 -0500

    add login and register routes

--- config/routes.js
+++ config/routes.js
@@ -1,30 +1,47 @@
-const axios = require('axios');
+const axios = require('axios')
 
-const { authenticate } = require('./middlewares');
+const { authenticate } = require('./middlewares')
+const db = require('../database/dbConfig.js')
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
-};
+  server.post('/api/register', register)
+  server.post('/api/login', login)
+  server.get('/api/jokes', authenticate, getJokes)
+}
 
 function register(req, res) {
-  // implement user registration
+  const { username, password } = req.body
+  const hash = bcrypt.hashSync(password, 4)
+  db('users')
+    .insert({ username, password: hash })
+    .then(id => res.status(201).json(id))
+    .catch(err => res.status(500).json(err))
 }
 
 function login(req, res) {
-  // implement user login
+  const { username, password } = req.body
+
+  db('users')
+    .where({ username })
+    .then(user => {
+      if (bcrypt.compareSync(password, user.password)) {
+        // generate token
+        // const token = generateToken({ ...user })
+        res.status(200).json({ message: 'you may pass', token })
+      } else {
+        res.status(401).json({ message: 'you shall not pass' })
+      }
+    })
+    .catch(err => res.status(500).json(err))
 }
 
 function getJokes(req, res) {
   axios
-    .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
-    )
+    .get('https://safe-falls-22549.herokuapp.com/random_ten')
     .then(response => {
-      res.status(200).json(response.data);
+      res.status(200).json(response.data)
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
-    });
+      res.status(500).json({ message: 'Error Fetching Jokes', error: err })
+    })
 }
